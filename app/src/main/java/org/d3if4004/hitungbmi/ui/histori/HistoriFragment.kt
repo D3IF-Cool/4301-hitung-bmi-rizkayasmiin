f45cb55c872f4301e244a31b42312589112768fb
Rizka_Amelia_Yasmin <rizkayasmin@student.telkomuniversity.ac.id>
2021-04-26 16:26:13 +0700

    Mengambil data dari database

--- app/src/main/java/org/d3if4004/hitungbmi/ui/histori/HistoriFragment.kt
+++ app/src/main/java/org/d3if4004/hitungbmi/ui/histori/HistoriFragment.kt
@@ -1,13 +1,24 @@
 package org.d3if4004.hitungbmi.ui.histori
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import org.d3if4004.hitungbmi.databinding.FragmentHistoriBinding
+import org.d3if4004.hitungbmi.db.BmiDb
 
 class HistoriFragment : Fragment() {
+
+    private val viewModel: HistoriViewModel by lazy {
+        val db = BmiDb.getInstance(requireContext())
+        val factory = HistoriViewModelFactory(db.dao)
+        ViewModelProvider(this, factory).get(HistoriViewModel::class.java)
+    }
+
     private lateinit var binding: FragmentHistoriBinding
 
     override fun onCreateView(
@@ -18,5 +29,13 @@ class HistoriFragment : Fragment() {
         binding = FragmentHistoriBinding.inflate(layoutInflater,
                     container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.data.observe(viewLifecycleOwner, {
+            Log.d("HistoriFragment", "Jumlah data: ${it.size}")
+        })
     }
 }
--- app/src/main/java/org/d3if4004/hitungbmi/ui/hitung/HitungViewModel.kt
+++ app/src/main/java/org/d3if4004/hitungbmi/ui/hitung/HitungViewModel.kt
@@ -16,10 +16,6 @@ class HitungViewModel(private val db: BmiDao) : ViewModel() {
     private val hasilBmi = MutableLiveData<HasilBmi?>()
     private val navigasi = MutableLiveData<KategoriBmi?>()
 
-    //variabel ini sudah berupa LiveData (tidak mutable),
-    //sehingga tidak perlu dijadikan private
-    val data = db.getLastBmi()
-
     fun hitungBmi(berat: String, tinggi: String, isMale: Boolean) {
         val tinggiCm = tinggi.toFloat() / 100
         val bmi = berat.toFloat() / (tinggiCm * tinggiCm)
